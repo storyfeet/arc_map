09f22b89d79f6c10c92ae30e39a27e9684425f9a
storyfeet <thebeaky@psmail.net>
2018-08-18 12:27:14 +0100

    now works, test still fails, as not sure how to wait for threads to finish

---
+++ src/amap_error.rs
@@ -0,0 +1,23 @@
+use std::sync::mpsc::{SendError,RecvError};
+use std::convert::From;
+
+#[derive(Debug)]
+pub enum AMapErr {
+    SendErr,
+    RecvErr,
+    NotFound,
+}
+
+
+
+impl<T> From<SendError<T>> for AMapErr{
+    fn from(_:SendError<T>)->Self{
+        AMapErr::SendErr
+    }
+}
+
+impl From<RecvError> for AMapErr{
+    fn from(_:RecvError)->Self{
+        AMapErr::RecvErr
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,47 +1,119 @@
-
 use std::collections::HashMap;
-use std::sync::mspc::{channel,Sender};
-use std::Thread;
+use std::sync::mpsc::{channel,Sender};
+use std::sync::{Arc,Mutex};
+use std::thread;
+use std::hash::Hash;
 
-enum Job<K,V> {
+pub mod amap_error;
+pub use amap_error::AMapErr;
+
+pub trait MKey : 'static +Send + Eq + Hash{}
+
+impl<K> MKey for K where K:'static+Send+Eq+Hash{}
+
+enum Job<K:MKey,V:Send> {
     Add(K,V,Sender<bool>),
-    Get(K,Sender<Arc<Mutex<V>>>),
+    Get(K,Sender<Option< Arc<  Mutex<V>  > >>),
     Remove(K),
-    Kill,
 }
 
 
-pub struct GuardMap<K,V>{
+#[derive(Clone)]
+pub struct GuardMap<K:MKey,V:Send>{
     ch:Sender<Job<K,V>>,
 }
 
-fn hidemap<K,V>()->Sender<Job<G>>{
+fn hide_map<K:MKey,V:'static+Send>()->Sender<Job<K,V>>{
     let (tx,rx) = channel();
     thread::spawn(move ||{
-        let mp:HashMap<usize,Arc<Mutex<G>>
+        let mut mp:HashMap<K,Arc<Mutex<V>>> = HashMap::new();
         loop {
-            
-            
+            match rx.recv(){
+                Ok(Job::Add(k,v,cbak))=>{
+                    mp.insert(k,Arc::new(Mutex::new(v)));
+                    cbak.send(true).unwrap_or(());
 
+                }
+                Ok(Job::Get(ref k,ref cbak))=>{
+                    match mp.get(k) {
+                        Some(ref a)=>cbak.send(Some((*a).clone())).unwrap_or(()),
+                        None=>cbak.send(None).unwrap_or(()),
+                    }
+                }
+                Ok(Job::Remove(ref k))=>{
+                    mp.remove(k);
+                }
+                _=>return,
+            }
         }
-
     });
-    
-
+    tx 
 }
 
-impl<G> GuardMap<G>{
-    pub fn new()->GuardMap<G>{
+impl<K:MKey,V:'static+Send> GuardMap<K,V>{
+    pub fn new()->GuardMap<K,V>{
         GuardMap{
-            v:Hashmap::new(),
-            ct:thread::spawn()
+            ch:hide_map(),
         }
     }
     
-    add(&mut self, G)->usize{
-        v:
+    pub fn add_key(&mut self, k:K,v:V)->Result<bool,AMapErr>{
+        let (tbak,rbak) = channel();
+        self.ch.send(Job::Add(k,v,tbak))?;
+        Ok(rbak.recv()?)
+    }
+
+    pub fn get(&mut self, k:K)->Result<Arc<Mutex<V>>,AMapErr>{
+        let (tbak,rbak) = channel();
+        self.ch.send(Job::Get(k,tbak))?;
+        match rbak.recv() {
+            Ok(Some(a))=>Ok(a),
+            Ok(None)=>Err(AMapErr::NotFound),
+            Err(d)=>Err(d.into()),
+        }
+    }
+
+    pub fn remove(&mut self, k:K)->Result<(),AMapErr>{
+        self.ch.send(Job::Remove(k))?;
+        Ok(())
+    }
+
+}
+
+
+
+#[cfg(test)]
+mod tests{
+    use super::*;
+    #[test]
+    fn add_to_ten(){
+        let mut mp = GuardMap::new();
+
+        for i in 0..10 {
+            mp.add_key(i,0).unwrap();
+        }
+
+        for i in 0 .. 10{
+            for _ in 0 .. 10 {
+                let n = i;
+                let m2 = mp.clone();
+                thread::spawn(move||{
+                    let g = m2.clone().get(n).unwrap();
+                    let mut incnum = g.lock().unwrap();
+                    *incnum +=1;
+                });
+            }
+        }
+
+        for i in 0..10 {
+            let g = mp.get(i).unwrap();
+            let num = g.lock().unwrap();
+            assert_eq!(*num,10);
+        }
+        
     }
 }
+
 
 
 
